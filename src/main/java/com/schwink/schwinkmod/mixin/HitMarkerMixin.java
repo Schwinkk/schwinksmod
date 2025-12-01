33d613c75066592e954c5df7bee5296ecf454db3
skhwink <78750945+Schwinkk@users.noreply.github.com>
2025-12-01 12:33:07 +0300

    added icon and finally got renderPipeline

--- src/main/java/com/schwink/schwinkmod/mixin/HitMarkerMixin.java
+++ src/main/java/com/schwink/schwinkmod/mixin/HitMarkerMixin.java
@@ -1,9 +1,13 @@
 package com.schwink.schwinkmod.mixin;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.schwink.schwinkmod.TreeCapitator;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -19,13 +23,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Item.class)
+@Mixin(Gui.class)
 public class HitMarkerMixin {
 
-    @Inject(method = "use", at = @At("HEAD"))
-    public void renderHitMarker(Level level, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir){
-        System.out.println("POEL");
+    private static final ResourceLocation HIT_ICON = ResourceLocation.fromNamespaceAndPath(TreeCapitator.MODID,"textures/gui/hit_marker");
+
+    public float iconOpacatity = 0f;
+
+    @Inject(method = "render", at = @At("TAIL"))
+    public void renderHitMarker(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci){
+        guiGraphics.blitSprite(RenderPipelines.GUI_TEXTURED,HIT_ICON,);
     }
 
+    public void showHitMarker(){
 
+    }
 }
