339123263853b15fdd06817634c196a804914990
Schwinkk <schwink.staff@exbo.ru>
2025-12-26 19:33:11 +0300

    some

--- src/main/java/com/schwink/schwinkmod/server/SchwinkModServer.java
+++ src/main/java/com/schwink/schwinkmod/server/SchwinkModServer.java
@@ -1,7 +1,11 @@
 package com.schwink.schwinkmod.server;
 
 import com.schwink.schwinkmod.common.Config;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.TagKey;
@@ -11,10 +15,12 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -28,6 +34,21 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 import java.util.Optional;
 
+public record HitData(String something) implements CustomPacketPayload{
+
+    public static final CustomPacketPayload.Type<HitData> TYPE = new
+
+    public static final StreamCodec<ByteBuf, HitData> STREAM_CODEC = StreamCodec.composite(
+        ByteBufCodecs.STRING_UTF8,
+        HitData::something,
+        HitData::new
+    );
+
+    @Override
+    public CustomPacketPayload.Type<? extends CustomPacketPayload> type(){
+        return TYPE;
+    }
+}
 
 @Mod(Config.MODID)
 public class SchwinkModServer {
@@ -55,7 +76,9 @@ public class SchwinkModServer {
         @SubscribeEvent
         private void onLivingAttack(LivingDamageEvent event){
             final DamageSource source = event.getEntity().getLastDamageSource();
-            if (source.getEntity().)
+            if (source!= null && source.getEntity() instanceof ServerPlayer player){
+                PacketDistributor.sendToPlayer();
+            }
         }
     }
 
